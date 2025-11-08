d941b1fcf6180337c8067207c26d934c1f9ad7e5
SCPD-PRODUCTION <scpdproduction4@gmail.com>
2025-11-08 11:42:50 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,11 @@
-// Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC5gAbdlbVL3t6oreb_ZZhAUT1YJVTKwPU",
   authDomain: "scpd-production.firebaseapp.com",
@@ -11,26 +18,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
-// Login Google resmi dengan popup
-function loginWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
-        .then(result => {
-            // Cek apakah user termasuk admin
-            const allowedAdmins = ["tsaqibys@gmail.com", "scpdproduct@gmail.com"]; // Ganti sesuai email admin
-            if(!allowedAdmins.includes(result.user.email)) {
-                alert("Akses ditolak! Hanya admin yang boleh login.");
-                auth.signOut();
-                return;
-            }
-            // Redirect ke dashboard
-            window.location.href = "dashboard.html";
-        })
-        .catch(error => {
-            console.error("Login gagal:", error);
-            alert("Login gagal, cek console.");
-        });
-}
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
